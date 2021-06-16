a9cc44cbcc66ffa56ffbc182e86902c50fdca87c
bjmontillon <pangilinanbj@gmail.com>
2021-06-16 12:57:47 +0800

    navlinks js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world!")
+
+var chevron = document.getElementsByClassName("fa-chevron-down")
+var i;
+for (let i = 0; i < chevron.length; i++) {
+    chevron[i].addEventListener("click", function() {
+        alert("bj");
+})
+}
