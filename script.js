84fb4269225aa6b33b03dbd71e33e27f83b17d63
bjmontillon <pangilinanbj@gmail.com>
2021-06-18 11:22:01 +0800

    accordion menu

--- script.js
+++ script.js
@@ -42,38 +42,13 @@ window.addEventListener('mouseup', function (eventConn) {
     }
 })
 
-    var bars1 = document.getElementById("bars1");
-    var bars2 = document.getElementById("bars2");
-    var bars3 = document.getElementById("bars3");
-
-document.getElementById("hamburgerMenu").addEventListener('click', function () {
-    var accordionMenu = document.getElementById("accordionMenu");
-    accordionMenu.style.visibility = "visible";
-})
-
-window.addEventListener('mouseup', function (eventAccor) {
-    var hamMenu = document.getElementById("hamburgerMenu");
-    var bars1 = document.getElementById("bars1");
-    var bars2 = document.getElementById("bars2");
-    var bars3 = document.getElementById("bars3");
-    if (eventAccor.target != hamMenu && eventAccor.target.parentNode != hamMenu) {
+var accordionMenu = document.getElementById("accordionMenu");
+var hamburgerMenu = document.getElementById("hamburgerMenu");
+hamburgerMenu.addEventListener('click', function () {
+    hamburgerMenu.classList.toggle("change");
+    if (accordionMenu.style.visibility === "visible") {
         accordionMenu.style.visibility = "hidden";
-        bars1.style.cssText = "-webkit-transform: rotate(0deg) translate(0px, 0px); transform: rotate(0deg) translate(0px, 0px);"
-        bars2.style.cssText = "opacity: 10;"
-        bars3.style.cssText = "-webkit-transform: rotate(0deg) translate(-0px, 0px); transform: rotate(0deg) translate(0px, 0px);"
+    } else {
+        accordionMenu.style.visibility = "visible";
     }
 })
-
-
-// function myFunction(x) {
-//     x.classList.toggle("change");
-// }
-
-
-function myFunction() {
-    // var bars1Style =(bars1).style.cssText = "-webkit-transform: rotate(-45deg) translate(-9px, 6px); transform: rotate(-45deg) translate(-5px, 4px);";
-    // bars1.style.toggle(bars1Style);'
-    bars1.classList.toggle("bars1Style");
-    bars2.classList.toggle("bars2Style");
-    bars1.classList.toggle("bars1Style");
-}
