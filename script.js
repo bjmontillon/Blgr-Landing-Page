3df9ae85d32fcd239444f86aefd7a2c53160fd72
bjmontillon <pangilinanbj@gmail.com>
2021-06-20 21:34:49 +0800

    section1

--- script.js
+++ script.js
@@ -68,22 +68,31 @@ hamburgerMenu.addEventListener('click', function () {
 
 // }
 //                      accordion menu                  //
+
+var fas1 = document.getElementById("fas1");
+var fas2 = document.getElementById("fas2");
+var fas3 = document.getElementById("fas3");
+
 accorProLink = () => {
-    let apLInk = document.getElementById("accor-pro-link");
-    let cmpLink = document.getElementById("accor-comp-link");
-    var mylink = document.getElementById("accor-conn-link")
+    var apLInk = document.getElementById("accor-pro-link");
+    var cmpLink = document.getElementById("accor-comp-link");
+    var mylink = document.getElementById("accor-conn-link");
+
     if(apLInk.style.display === "block") {
         apLInk.style.display = "none";
     } else {
         apLInk.style.display = "block";
         cmpLink.style.display = "none";
         mylink.style.display = "none";
+        fas1.style.cssText = "-webkit-transform: rotateX(180deg);transform: rotateX(180deg)";
+        fas2.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
+        fas3.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
     }
 }
 
 accorCompLink = () => {
-    let cmpLink = document.getElementById("accor-comp-link");
-    let apLInk = document.getElementById("accor-pro-link");
+    var cmpLink = document.getElementById("accor-comp-link");
+    var apLInk = document.getElementById("accor-pro-link");
     var mylink = document.getElementById("accor-conn-link")
     
     if (cmpLink.style.display === "block") {
@@ -92,19 +101,25 @@ accorCompLink = () => {
         cmpLink.style.display = "block";
         mylink.style.display = "none";
         apLInk.style.display = "none";
+        fas2.style.cssText = "-webkit-transform: rotateX(180deg);transform: rotateX(180deg)";
+        fas1.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
+        fas3.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
     }
  }
 
 accorConnLink = () => {
     var mylink = document.getElementById("accor-conn-link")
-    let cmpLink = document.getElementById("accor-comp-link");
-    let apLInk = document.getElementById("accor-pro-link");
+    var cmpLink = document.getElementById("accor-comp-link");
+    var apLInk = document.getElementById("accor-pro-link");
     if (mylink.style.display === "block") {
         mylink.style.display = "none";
     } else {
         mylink.style.display = "block";
         apLInk.style.display = "none";
         cmpLink.style.display = "none"
+        fas3.style.cssText = "-webkit-transform: rotateX(180deg);transform: rotateX(180deg)";
+        fas1.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
+        fas2.style.cssText = "-webkit-transform: rotateX(0deg);transform: rotateX(0deg)";
     }
 }
 
